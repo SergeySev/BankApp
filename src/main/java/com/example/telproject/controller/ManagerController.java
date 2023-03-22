2e1fe3966d6f74c28a2bc175f5ef29b1a532d8ef
SergeySev <1sever6iv@gmail.com>
2023-03-22 17:13:58 +0100

    write DTO for ManagerDTO.java

--- src/main/java/com/example/telproject/controller/ManagerController.java
+++ src/main/java/com/example/telproject/controller/ManagerController.java
@@ -7,7 +7,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/managers")
--- src/main/java/com/example/telproject/service/ManagerServiceInterface.java
+++ src/main/java/com/example/telproject/service/ManagerServiceInterface.java
@@ -1,9 +1,5 @@
 package com.example.telproject.service;
 
-import com.example.telproject.entity.Manager;
-
-import java.util.List;
-
 public interface ManagerServiceInterface {
 
 }
