10463dd779831f86fd484ddcdddeb578177b5f59
SergeySev <1sever6iv@gmail.com>
2023-04-20 10:27:12 +0200

    Debug quantity photos on the page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -15,30 +15,7 @@ const ClientProfiles = () => {
         fetchClientProfiles();
     }, []);
 
-    const createImageFromBase64 = (base64String, format) => {
-        const img = new Image();
-        img.src = `data:image/${format};base64,${base64String}`;
-        document.body.appendChild(img);
-    };
-
-    const getImageUrl = async (client_id) => {
-        const response = await axios.get("http://localhost:8080/api/v1/client-document/document?client_id=" + client_id);
-        let i = 0;
-        while (response.data[i].document !== undefined) {
-            console.log("Client ID is " + client_id);
-            console.log("I : " + i);
-            console.log(response.data[i]);
-
-            const base64String = response.data[i].document;
-            const format = response.data[i].type;
-
-            createImageFromBase64(base64String, format);
-            i++;
-        }
-    };
-
     return clientProfiles.map((clientProfile, index) => {
-        getImageUrl(index + 1);
         return (
             <div key={index}>
                 <h3>Name: {clientProfile.first_name} {clientProfile.last_name}</h3>
@@ -47,25 +24,41 @@ const ClientProfiles = () => {
                 <p>Email: {clientProfile.email}</p>
                 <p>Created at: {clientProfile.created_at}</p>
                 <MyDropzone {...clientProfile.id}/>
+                <ShowImage {...clientProfile.id}/>
                 <br/>
             </div>
         )
     });
-
-    return clientProfiles.map((clientProfile, index) => {
-        return (
-            <div key={index}>
-                <h3>Name: {clientProfile.first_name} {clientProfile.last_name}</h3>
-                <p>Id: {clientProfile.id}</p>
-                <p>Status: {clientProfile.status}</p>
-                <p>Email: {clientProfile.email}</p>
-                <p>Created at: {clientProfile.created_at}</p>
-                <MyDropzone {...clientProfile.id}/>
-                <br/>
-            </div>
-        )
-    })
 };
+
+function ShowImage(client_id) {
+    const [loadedImages] = useState([]);
+    const createImageFromBase64 = (base64String, format) => {
+        if (!loadedImages.includes(base64String)) {
+            const img = new Image();
+            img.src = `data:image/${format};base64,${base64String}`;
+            document.body.appendChild(img);
+            loadedImages.push(base64String);
+        }
+    };
+
+    useEffect(() => {
+        const getImageUrl = async () => {
+            const response = await axios.get("http://localhost:8080/api/v1/client-document/document?client_id=" + client_id[0]);
+            let i = 0;
+            while (i < response.data.length) {
+                const base64String = response.data[i].document;
+                const format = response.data[i].type;
+
+                createImageFromBase64(base64String, format);
+                i++;
+            }
+        };
+        getImageUrl();
+    }, [client_id, loadedImages]);
+
+    return null;
+}
 
 function MyDropzone(client_id) {
     const onDrop = useCallback(acceptedFiles => {
